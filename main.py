ba95922f97f270a0bb9cbea8e44012411775238c
martan3d <martan3d@cstone.net>
2019-02-10 03:31:34 +0000

    revisions for fits to phones

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ main_api = Blueprint('main_api', __name__)
 
 NOP = "sent"
 GLOB = 'G'
+HEIGHT = 0
+WIDTH = 0
+
+@main_api.route('/setsize/', methods=['GET','POST'])
+def setsize():
+    global HEIGHT
+    global WIDTH
+
+    HEIGHT = request.form['height']
+    WIDTH  = request.form['width']
+
+    print HEIGHT, WIDTH
+
+    return NOP
+
 
 # main entry point, used on load page
 @main_api.route('/main/', methods=['GET','POST'])
@@ -18,9 +33,14 @@ def main():
 # API entry point from web page javascript refresh code
 @main_api.route('/refresh/', methods=['GET','POST'])
 def refresh():
+    global HEIGHT
+    global WIDTH
 
-    data = '<svg id="svg1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1000" height="1000">'
-    data = data + '<style>.myfont { font: 20px sans-serif; } </style>'
+    w = int(WIDTH)
+    h = int(HEIGHT) * 2
+
+    data = '<svg id="svg1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="%d" height="%d">' % (w, h)
+    data = data + '<style>.myfont { font:20px sans-serif; } .afont { font:50px sans-serif; } </style>'
 
     data = data + drawNodes(data)
     data = data + "</svg>"
@@ -28,7 +48,16 @@ def refresh():
 
 # draw the big scan button at the top of the screen
 def drawNodes(data):
-    x = 10
+    global HEIGHT
+    global WIDTH
+
+    my = int(HEIGHT)
+    mx = int(WIDTH)
+
+    x = mx/4
+    my = my/4
+
+    #x = 10
     y = 10
     db = MySQLdb.connect(host="localhost", passwd="raspberry", db="webapp")
     cr = db.cursor()
@@ -37,27 +66,36 @@ def drawNodes(data):
     results = cr.fetchall()
     for r in results:
         data = data + drawBox(x, y, r[0],r[1],r[2],r[3],r[4])
-        y = y + 110
+        y = y + my
     return data
 
 def drawBox(x, y, id, name, address, type, status):
+    global WIDTH
+
+    mywidth = int(WIDTH)
+    myheight = int(HEIGHT)
+
+    mywidth = mywidth/2
+    center  = mywidth
+    myheight = myheight/4 - 40
+
     data = '''
         <rect
          fill="lightgray"
          style="stroke:black;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-         id="scanbutton" width="200" height="80"
+         id="scanbutton" width="%d" height="%d"
          x="%s" y="%s" rx="6" ry="6"
          onclick="javascript:getNode('%s','%s');"
-        />''' % ( x, y, address, name)      #'''
+        />''' % ( mywidth, myheight, x, y, address, name)      #'''
 
-    data = data + '<text x="%s" y="%s" font-size="28" class="myfont" fill="black" ' % (x+10, y+30 )
+    data = data + '<text x="%s" y="%s" class="afont" fill="black" ' % (x+20, y+60 )
     data = data + 'onclick="javascript:getNode('
     data = data + "'%s','%s');" % (address, name)
     data = data + '">'
     data = data + '%s' % name
     data = data + '</text>'
 
-    data = data + '<text x="%s" y="%s" font-size="22" class="myfont" fill="black" ' % (x+10, y+60)
+    data = data + '<text x="%s" y="%s" class="myfont" fill="black" ' % (x+20, y+90)
     data = data + 'onclick="javascript:getNode('
     data = data + "'%s','%s');" % (address, name)
     data = data + '">'
@@ -77,13 +115,19 @@ def scannetwork():
 @main_api.route('/displaynode/', methods=['POST','GET'])
 def displayNode():
     global GLOB
+    global HEIGHT
+    global WIDTH
+
+    my = int(HEIGHT)
+    mx = int(WIDTH)
+
     address = request.form['address']
     name    = request.form['name']
     senddata = base64.b64encode(json.dumps(['READNODE', address, '01234567890123456789']))
     r = redis.Redis(host='127.0.0.1', port='6379')
     r.rpush(['queue:xbeetx'], senddata )
     GLOB = 'G'
-    return render_template('node.html', address=address, name=name)
+    return render_template('node.html', address=address, name=name, x=mx, y=my)
 
 @main_api.route('/setcv/', methods=['POST','GET'])
 def setcv():
@@ -114,6 +158,7 @@ def setdcc():
     SETDCCADDR = 40
     datapayload = chr(SETDCCADDR) + dccaddr[0] + dccaddr[1] + dccaddr[2] + dccaddr[3] + '567890123456789'
     senddata = base64.b64encode(json.dumps(['SETDCC', address, datapayload ]))
+
     r = redis.Redis(host='127.0.0.1', port='6379')
     r.rpush(['queue:xbeetx'], senddata )
 
@@ -237,51 +282,100 @@ def refreshnode():
     data = data + '<input type="hidden" id="address" value="%s">' % address
 
     data = data + '''
-       <table style="margin-left:20px;">
+       <table align=center>
+
        <td>ProtoThrottle ID</td>
        <td style="width:20px;"> &nbsp; </td>
-       <td style="width:44px;"><input style="text-align:right;font-size:18px;width:50px;padding-right:4px;" type="text" id="pid" value="0"></td>
-       <td><input class="pbutton" type="button" onclick="setProto();" value="P"></td>
+       <td style="width:44px;"><input class="myinput" type="text" id="pid" value="0"></td>
+       <td><input class="theButton" type="button" onclick="setProto();" value="Prg"></td>
        <tr>
+
        <td>Base ID</td>
        <td> &nbsp; </td>
-       <td><input style="text-align:right;font-size:18px;width:50px;padding-right:4px;" type="text" id="bid" value="A"></td>
-       <td><input  class="pbutton" type="button" onclick="setBase();" value="P"></td>
-       <tr>'''
+       <td><input class="myinput" type="text" id="bid" value="A"></td>
+       <td><input  class="theButton" type="button" onclick="setBase();" value="Prg"></td>
+       <tr>
 
-    if nodetype == 'W':
-       data = data + '''
        <td>Loco Address</td>
        <td> &nbsp; </td>
-       <td><input style="text-align:right;font-size:18px;width:50px;padding-right:4px;" type="text" id="dccaddr" value="3"></td>
-       <td><input  class="pbutton" type="button" id="dccaddr" onclick="setMaster();" value="P"></td>
+       <td><input class="myinput" type="text" id="dccaddr" value="3"></td>
+       <td><input  class="theButton" type="button" id="dccaddr" onclick="setMaster();" value="Prg"></td>
        <tr>
-       <td>Consist</td>
-       <td><div id="direction" style="background-color:white;border:1px solid black;cursor:pointer;padding:2px;text-align:center;" onclick="setConsDir();"/> F </div>
-       <input type="hidden" id="consistdir" value="F"></td>
-       <td><input style="text-align:right;font-size:18px;width:50px;padding-right:4px;" type="text" id="consistaddr" value="3"></td>
-       <td><input  class="pbutton" type="button" onclick="setConsist();" value="P"></td>
-       '''
-    if nodetype == 'A':
-       data = data + '''
-       <td>Airwire Channel</td><td></td><td><input style="text-align:right;margin-left:40px;font-size:18px;width:50px;padding-right:4px;" type="text" id="airaddr" value="3"></td>
-       <td><input  class="pbutton" type="button" onclick="setAirwire();" value="P"></td>
-       '''
-    data = data + '''
-       </table>
-    </div>'''
 
-    if nodetype == 'W':
-       data = data + '''
+       <td>Consist</td>
+       <td><div id="direction" onclick="setConsDir();"/> F </div>
+       <input type="hidden" id="consistdir" value="F"></td>
+       <td><input class="myinput" type="text" id="consistaddr" value="3"></td>
+       <td><input  class="theButton" type="button" onclick="setConsist();" value="Prg"></td>
+       <tr>
+
+       <td> &nbsp; </td><td style="font-size:10px;text-align:center;">CVaddr</td><td style="font-size:10px;text-align:center;">CVdata</td><td> &nbsp; </td>
+       <tr>
+       <td>CVProg</td>
+       <td><input type="text" id="cvaddr" class="myinput"></td>
+       <td><input type="text" id="cvdata" class="myinput"></td>
+       <td><input class="theButton" type="button" onclick="setCV();" value="Prg"></td>
+       <tr>
+
+       <td> &nbsp; </td><td style="font-size:10px;text-align:center;">LoLim</td><td style="font-size:10px;text-align:center;">HiLim</td><td> &nbsp; </td>
+       <tr>
+       <td>Servo 0</td>
+       <td><input type="text" id="slo0" class="myinput"></td>
+       <td><input type="text" id="shi0" class="myinput"></td>
+       <td><input class="theButton" type="button" onclick="setServo(0);" value="Prg"></td>
+       <tr>
+
+       <td>Servo 1</td>
+       <td><input type="text" id="slo1" class="myinput"></td>
+       <td><input type="text" id="shi1" class="myinput"></td>
+       <td><input class="theButton" type="button" onclick="setServo(1);" value="Prg"></td>
+       <tr>
+
+       <td>Servo 2</td>
+       <td><input type="text" id="slo2" class="myinput"></td>
+       <td><input type="text" id="shi2" class="myinput"></td>
+       <td><input class="theButton" type="button" onclick="setServo(2);" value="Prg"></td>
+       <tr>
+
+       </table>
+
+           <div style="text-align:center;margin-top:20px;">
+             <input  class="mybutton" type="button" onclick="setHome();" value="Home">
+           </div>'''
+
+    return data
+
+
+    d = d + '''
          <div style="font-size:22px;text-align:center;margin-top:10px;">
-           <div style="text-align:center;margin-top:16px;">
+           <div style="text-align:center;margin-top:10px;">
              <span style="position:block;font-size:10px;margin-right:28px;">CVaddr</span> <span style="position:block;font-size:10px;margin-right:62px;">CVdata</span>
            </div>
-           <div style="text-align:center;margin-top:10px;">
+           <div style="text-align:center;margin-top:8px;">
                <input type="text" id="cvaddr" style="width:54px;height:32px;font-size:20px;margin-right:8px;margin-left:8px;">
                <input type="text" id="cvdata" style="width:54px;height:32px;font-size:20px;margin-right:8px;">
                <input class="theButton" type="button" style="position:relative;top:50%;transform:translateY(-8%);" onclick="setCV();" value="Prg">
            </div>
+           <div style="text-align:center;margin-top:1px;">
+             <span style="position:block;font-size:10px;margin-right:20px;">LowLim</span> <span style="position:block;font-size:10px;margin-right:64px;">HighLim</span>
+           </div>
+           <div style="text-align:center;margin-top:1px;">
+               <div style="margin-left:-20px;display:inline;">0</div>
+               <input type="text" id="cvaddr" style="width:54px;height:32px;font-size:20px;margin-right:8px;margin-left:8px;">
+               <input type="text" id="cvdata" style="width:54px;height:32px;font-size:20px;margin-right:8px;">
+               <input class="theButton" type="button" style="position:relative;top:50%;transform:translateY(-8%);" onclick="setCV();" value="Prg">
+           </div>
+
+           <div style="text-align:center;margin-top:1px;">
+             <span style="position:block;font-size:10px;margin-right:20px;">LowLim</span> <span style="position:block;font-size:10px;margin-right:64px;">HighLim</span>
+           </div>
+           <div style="text-align:center;margin-top:1px;">
+               <div style="margin-left:-20px;display:inline;">1</div>
+               <input type="text" id="cvaddr" style="width:54px;height:32px;font-size:20px;margin-right:8px;margin-left:8px;">
+               <input type="text" id="cvdata" style="width:54px;height:32px;font-size:20px;margin-right:8px;">
+               <input class="theButton" type="button" style="position:relative;top:50%;transform:translateY(-8%);" onclick="setCV();" value="Prg">
+           </div>
+
            <div style="text-align:center;margin-top:20px;">
              <input  class="mybutton" type="button" onclick="setHome();" value="Home">
            </div>
